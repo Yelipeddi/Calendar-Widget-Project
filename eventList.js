3389b54197d6a0594d3d74faac30b37e245bae36
Yelipeddi <31531302+Yelipeddi@users.noreply.github.com>
2018-02-10 17:54:54 -0800

    Add files via upload

---
+++ eventList.js
@@ -0,0 +1,21 @@
+// This is the Events Database
+// Each Event has 5 fields 
+// Syntax for each event is as below
+//["EventID", "day", "Event Name", "Event Description", "Start Time", "End Time"]
+// Each event has a comma after the closing bracket If another event is below it on the next line down.
+
+
+events = new Array(
+	
+	["1", "15", "Stakeholders Meeting", "If Meeting with Stakeholders to discuss about project requirements", "9:00 AM", "10:00 AM"],
+        ["2", "15", "Lunch", "Team lunch at ABC restaurant", "12:00 PM", "1:00 PM"],
+        ["3", "15", "Bug scrub", "Meeting to analyze the release blocking software bugs", "4:00 PM", "5:00 PM"],
+        ["4", "16", "Lunch", "Lunch meeting with Customers", "12:00 PM", "1:00 PM"],
+        ["5", "16", "Training", "Mandatory training", "10:00 AM", "1:00 PM"],
+        ["6", "19", "Company All Hands", "Company All hands meeting to discuss the quarterly results", "3:00 PM", "5:00 PM"]   
+	
+);
+
+
+
+
